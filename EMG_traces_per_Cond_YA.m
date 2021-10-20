3965495eb68c5cc483b7000af032a66b053e72c8
DulceMariscal <mariscaldulce@gmail.com>
2021-10-20 12:17:12 -0400

    Script to plot the EMG traces for different conditons

--- EMG_traces_per_Cond_YA.m
+++ EMG_traces_per_Cond_YA.m
@@ -1,1 +1,54 @@
+%%Traces from example subject to show how data is summarized
+%% Load data
+% load('/Volumes/Users/Dulce/R01_Nimbus2021/VROG_Devon/VrG_Devon.mat')
+% subID = 'CTR_01';
+% scriptDir = fileparts(matlab.desktop.editor.getActiveFilename); 
+% load([scriptDir '/data/' subID])
+
+%% Set muscle to plot
+
+muscle={'TA', 'PER', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'TFL', 'GLU','HIP'};
+normalize = 1;  % 1 to normalize data
+normCond = {'TM base'};
+
+%% Baseline condtions 
+conds={'OG base','TM fast','TM base','TM slow'};
+late=1;
+strides=40;
+fh=plotEMGtraces(expData,conds,muscle,late,strides,normalize,normCond);
+
+%% Late condition 
+late=1;
+strides=40;
+conds={'OG base','TM fast','TM slow'...
+    'TM base','Adaptation',...
+    'TM post','OG post'};
+fh=plotEMGtraces(expData,conds,muscle,late,strides,normalize,normCond);
+
+
+%% Early conditions 
+late=0;
+strides=30;
+conds={'TM base','Pos short',...
+    'Neg Short','Adaptation',...
+    'TM post','OG post'};
+fh=plotEMGtraces(expData,conds,muscle,late,strides,normalize,normCond);
+
+%% Early conditions 
+late=0;
+strides=30;
+conds={'Pos short',...
+    'Neg Short','Adaptation',...
+        };
+fh=plotEMGtraces(expData,conds,muscle,late,strides,normalize,normCond);
+
+%% save figures
+% if late
+%     if baseOnly
+%         saveas(fh, [scriptDir '/EMGTraces/' subID '_BaseLate.png']);
+%     else
+%         saveas(fh, [scriptDir '/EMGTraces/' subID '_Late.png']);
+%     end
 % else
+%     saveas(fh, [scriptDir '/EMGTraces/' subID '_Early.png']);
+% end
