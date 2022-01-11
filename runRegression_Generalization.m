73ecc383c3485345e6c6a9a50ac495fc32d6303d
DulceMariscal <mariscaldulce@gmail.com>
2022-01-11 17:00:35 -0500

    Short split transition and stepwise regression
    - Adding the model to do the regression for the short split
    - Turning off the stepwise regression

--- runRegression_Generalization.m
+++ runRegression_Generalization.m
@@ -1,11 +1,11 @@
-function [fitTrans1NoConst,fitTrans2NoConst]=runRegression_Generalization(Data, normalizeData, isGroupData, dataId, resDir, saveResAndFigure, version, usefft, regressorNames) 
+function [fitTrans1NoConst,fitTrans2NoConst,fitTrans3NoConst]=runRegression_Generalization(Data, normalizeData, isGroupData, dataId, resDir, saveResAndFigure, version, usefft, regressorNames);
 % perform regression anlysis V2 (see grant one note: Regression discussion (two transitions)
 % printout the regression results and save the results to destination
 % folders (if saveResAndFigure flag is on)
 % ----- Arguments ------
-% - Data: a 1x5 cell where each cell contains a 12x28 matrix. The cell
-% corresponds to data for: adapt, envSwitch, taskSwitch, transition1 and
-% transition 2. The matrix size might differ due to removal of bad data.
+% - Data: a 1x6 cell where each cell contains a 12x28 matrix. The cell
+% corresponds to data for: adapt, envSwitch, taskSwitch, transition1,
+% transition 2, transition 3. The matrix size might differ due to removal of bad data.
 % - normalizeData: boolean flag of whether or not to normalize the vector
 % (regressor) by the length
 % - isGroupData: boolean flag indicating the regression is for individual
@@ -23,117 +23,145 @@ function [fitTrans1NoConst,fitTrans2NoConst]=runRegression_Generalization(Data, 
 % - usefft: OPTIONAL, boolean flag indicating if should use fft of the data to
 % approximate deltaOn-, default false.
 % - regressorNames: OPTIONAL, a cell array of the regressor names, size of
-% 1 x 5, default name: {'Adapt','WithinContextSwitch','MultiContextSwitch','Trans1','Trans2'}
-% 
+% 1 x 6, default name: {'Adapt','WithinContextSwitch','MultiContextSwitch','Trans1','Trans2','Trans3'}
+%
 % ----- Returns ------
 %  none
-% 
-    if nargin < 7 || isempty(version)
-        version = 'default'; %default 1
-    end
-    if nargin < 8 || isempty(usefft)
-        usefft = false; %default false
-    end
-    if nargin < 9
-        regressorNames = {'Adapt','WithinContextSwitch','MultiContextSwitch','Trans1','Trans2'}; %default names
-    end
-    
-    if ~isGroupData
-        for i = 1:size(Data,2)
-            Data{i} = reshape(Data{i}, [],1); %make it a column vector
-        end
-    else %group data, take the median
-        for i = 1:size(Data,2)
-            d = nanmedian(Data{i}, 4);
-            Data{i} = reshape(d, [],1); %make it a column vector
-        end
-    end
+%
+if nargin < 7 || isempty(version)
+    version = 'default'; %default 1
+end
+if nargin < 8 || isempty(usefft)
+    usefft = false; %default false
+end
+if nargin < 9
+    regressorNames = {'Adapt','Noadapt','ContextSwitch','Trans1','Trans2','Trans3'}; %default names
+end
 
-    if usefft %do fft - run only once
-        Data{size(Data,2) + 1} = Data{1}; %store the current on to the last
-        Data{1} = fftshift(Data{1},1);
+if ~isGroupData
+    for i = 1:size(Data,2)
+        Data{i} = reshape(Data{i}, [],1); %make it a column vector
     end
-    
-    fprintf('\n\n\n')
-    normalizeData
-    if normalizeData
-        for i = 1:size(Data,2)
-            Data{i} = Data{i}/norm(Data{i});
-        end
+else %group data, take the median
+    for i = 1:size(Data,2)
+        d = nanmedian(Data{i}, 4);
+        Data{i} = reshape(d, [],1); %make it a column vector
     end
+end
 
-    %define model based on the version, version has to fall in 1 of the 3
-    %categories, otherwise there is a bug in the code.
-    if strcmpi(version,'default') %default, 3 regressors version
-        trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '+' regressorNames{3} '-1'];
-        trans2Model = [regressorNames{5} '~' regressorNames{1} '+' regressorNames{2} '+' regressorNames{3} '-1'];
-%     elseif strcmpi(version,'tr') %training group
-%         trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '-1'];
-%         trans2Model = [regressorNames{5} '~' regressorNames{1} '+' regressorNames{3} '-1'];
-%     elseif strcmpi(version,'ts') %testing group
-%         trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '-1'];
-%         trans2Model = [regressorNames{5} '~' regressorNames{1} '+' regressorNames{3} '-1'];
-    elseif  strcmpi(version,'Adaptive_EnvTransition') %testing group
-        trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '-1'];
-        trans2Model = [regressorNames{5} '~' regressorNames{2} '+' regressorNames{3} '-1'];
-        
+if usefft %do fft - run only once
+    Data{size(Data,2) + 1} = Data{1}; %store the current on to the last
+    Data{1} = fftshift(Data{1},1);
+end
+
+fprintf('\n\n\n')
+normalizeData
+if normalizeData
+    for i = 1:size(Data,2)
+        Data{i} = Data{i}/norm(Data{i});
     end
-    
-    %%% Run regression analysis V2
-    tableData=table(Data{1},Data{2},Data{3},Data{4},Data{5},'VariableNames',regressorNames);
-    fitTrans1NoConst=fitlm(tableData,trans1Model)%exclude constant
-    
-    Rsquared = fitTrans1NoConst.Rsquared
-    %compute adaptation and switch index
-    beta1_index = computeBetaIndex(fitTrans1NoConst);
-    
-    fprintf('\n\n')
+end
 
-    fitTrans2NoConst=fitlm(tableData,trans2Model)%exclude constant
-    Rsquared = fitTrans2NoConst.Rsquared
-    %compute adaptation and switch index
-    beta2_index = computeBetaIndex(fitTrans2NoConst);
+%define model based on the version, version has to fall in 1 of the 3
+%categories, otherwise there is a bug in the code.
+if strcmpi(version,'default') %default, 3 regressors version
+    trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '+' regressorNames{3} '-1'];
+    trans2Model = [regressorNames{5} '~' regressorNames{1} '+' regressorNames{2} '+' regressorNames{3} '-1'];
+    trans3Model = [regressorNames{6} '~' regressorNames{1} '+' regressorNames{2} '+' regressorNames{3} '-1'];
+    %     elseif strcmpi(version,'tr') %training group
+    %         trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '-1'];
+    %         trans2Model = [regressorNames{5} '~' regressorNames{1} '+' regressorNames{3} '-1'];
+    %     elseif strcmpi(version,'ts') %testing group
+    %         trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '-1'];
+    %         trans2Model = [regressorNames{5} '~' regressorNames{1} '+' regressorNames{3} '-1'];
+elseif  strcmpi(version,'Adaptive_EnvTransition') %testing group
+    trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '-1'];
+    trans2Model = [regressorNames{5} '~' regressorNames{2} '+' regressorNames{3} '-1'];
+% elseif strcmpi(version,'Adaptive_Feedback') %testing group
+%     trans1Model = [regressorNames{4} '~' regressorNames{1} '+' regressorNames{2} '+' regressorNames{3} '+' regressorNames{6} '-1'];
+%     trans2Model = [regressorNames{5} '~' regressorNames{1} '+' regressorNames{2} '+' regressorNames{3} '+' regressorNames{6} '-1'];
+%     
+end
 
-    %compute and print out relative vector norm to assess the length
-    %difference between regressors
-    fprintf('\n\n')
-    vec_norm = vecnorm(fitTrans1NoConst.Variables{:,:});
-    relNom = normalize(vec_norm,'norm',1)
+%%% Run regression analysis V2
+tableData=table(Data{1},Data{2},Data{3},Data{4},Data{5},Data{6},'VariableNames',regressorNames);
+fitTrans1NoConst=fitlm(tableData,trans1Model)%exclude constant
+
+Rsquared = fitTrans1NoConst.Rsquared
+%compute adaptation and switch index
+beta1_index = computeBetaIndex(fitTrans1NoConst);
+
+fprintf('\n\n')
+
+fitTrans2NoConst=fitlm(tableData,trans2Model)%exclude constant
+Rsquared = fitTrans2NoConst.Rsquared
+%compute adaptation and switch index
+beta2_index = computeBetaIndex(fitTrans2NoConst);
+
+fprintf('\n\n')
+
+fitTrans3NoConst=fitlm(tableData,trans3Model)%exclude constant
+Rsquared = fitTrans3NoConst.Rsquared
+%compute adaptation and switch index
+beta3_index = computeBetaIndex(fitTrans3NoConst);
+
+
+
+%compute and print out relative vector norm to assess the length
+%difference between regressors
+fprintf('\n\n')
+vec_norm = vecnorm(fitTrans1NoConst.Variables{:,:});
+relNom = normalize(vec_norm,'norm',1)
+
+fprintf('\n\n')
+%Stepwise regression
+if strcmpi(version,'default')
     
-    fprintf('\n\n')
-    %Stepwise regression 
     X=[Data{1},Data{2},Data{3}];
-    z= [Data{1},Data{2},Data{3},Data{4},Data{5}];
-    
+elseif strcmpi(version,'Adaptive_EnvTransition')
+     X=[Data{1},Data{2}];
+
+% elseif  strcmpi(version,'Adaptive_Feedback')
+%      X=[Data{1},Data{2},Data{3},Data{6}];
+end
+z= [Data{1},Data{2},Data{3},Data{4},Data{5},Data{6}];
 %     corrcoef(X,'Rows','complete')
-    disp('Correlation betwen regressor and dependent variables')
-    corrcoef(z,'Rows','complete')
-    
-    fprintf('\n\n')
-    disp('Colinearity between the regressors')
-    vif(X)
-    
-    Y1=Data{4};
-    Y2=Data{5};
-    fprintf('\n\n')
-    disp('Transition 1')
-    [B,SE,PVAL,INMODEL,STATS]=stepwisefit(X,Y1,'PRemove',0.05)
-    
-    fprintf('\n\n')
-    disp('Transition 2')
-    [B2,SE2,PVAL2,INMODEL2,STATS2]=stepwisefit(X,Y2, 'PRemove',0.05)
-    
-    if saveResAndFigure
-        if not(isfolder(resDir))
-            mkdir(resDir)
-        end
-        if ~isGroupData
-            save([resDir dataId 'models_ver' num2str(usefft) num2str(normalizeData)], 'fitTrans1NoConst','fitTrans2NoConst','beta1_index','beta2_index','relNom');
-        else
-            %version convention: first digit: use first or last stride, 2nd digit:
-            %use fft or not, 3rd digit: normalize or not, i.e., ver_101 = use first
-            %20 strides, no fft and normalized data
-            save([resDir dataId '_group_models_ver' num2str(usefft) num2str(normalizeData)], 'fitTrans1NoConst','fitTrans2NoConst','beta1_index','beta2_index','relNom');
-        end
+disp('Correlation betwen regressor and dependent variables')
+corrcoef(z,'Rows','complete')
+
+fprintf('\n\n')
+disp('Colinearity between the regressors')
+    vif(z)
+%     collintest(z)
+%     collintest(tableData)
+
+% Y1=Data{4};
+% Y2=Data{5};
+% Y3=Data{6};
+% 
+% fprintf('\n\n')
+% disp('Transition 1')
+% [B,SE,PVAL,INMODEL,STATS]=stepwisefit(X,Y1,'PRemove',0.05)
+% 
+% fprintf('\n\n')
+% disp('Transition 2')
+% [B2,SE2,PVAL2,INMODEL2,STATS2]=stepwisefit(X,Y2, 'PRemove',0.05)
+% 
+% fprintf('\n\n')
+% disp('Transition 3')
+% [B2,SE2,PVAL2,INMODEL2,STATS2]=stepwisefit(X,Y3, 'PRemove',0.05)
+
+if saveResAndFigure
+    if not(isfolder(resDir))
+        mkdir(resDir)
     end
+    if ~isGroupData
+        save([resDir dataId 'models_ver' num2str(usefft) num2str(normalizeData)], 'fitTrans1NoConst','fitTrans2NoConst','fitTrans3NoConst','beta1_index','beta2_index','beta3_index','relNom');
+    else
+        %version convention: first digit: use first or last stride, 2nd digit:
+        %use fft or not, 3rd digit: normalize or not, i.e., ver_101 = use first
+        %20 strides, no fft and normalized data
+        save([resDir dataId '_group_models_ver' num2str(usefft) num2str(normalizeData)], 'fitTrans1NoConst','fitTrans2NoConst','fitTrans3NoConst','beta1_index','beta2_index','beta3_index','relNom');
+    end
+end
 end
