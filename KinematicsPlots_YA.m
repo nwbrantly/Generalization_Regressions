2ab8e7651144326eb4ab9532d3286d52eed8453f
DulceMariscal <mariscaldulce@gmail.com>
2022-01-11 17:00:54 -0500

    Script to plot kinematics

--- KinematicsPlots_YA.m
+++ KinematicsPlots_YA.m
@@ -1,15 +1,20 @@
 
-group{1}=adaptationData.createGroupAdaptData({'ATR01params','ATR02params','ATR03params'});
+group{1}=adaptationData.createGroupAdaptData({'ATR01params','ATR02params','ATR03params','ATR04params'});
+% group{1}=adaptationData.createGroupAdaptData({'ATR01paramsForces','ATR02paramsForces','ATR03paramsForces'});
+group{1}=adaptationData.createGroupAdaptData({'ATS02params','ATS03params','ATS04params','ATS05params',});
 
-% conditions={'OG base','TM slow','TM fast','TM base','Adaptation','Post 1','Post 2','TM mid 1','Pos Short','TM mid 2','Neg Short','TM mid 3'};
+conditions={'OG base','TM slow','TM fast','TM base','Adaptation','Post 1','Post 2','TM mid 1','Pos Short','TM mid 2','Neg Short','TM mid 3'};
 
-conditions={'TM base','Adaptation','Post 1'};
+conditions={'OG base','TM slow','TM fast','TM base','Adaptation','Post 1','Post 2','TM mid 1','Pos Short','OG 1','TM mid 3','Neg Short','OG 2'};
+% 
+% conditions={'TM base','Adaptation','Post 1'};
 
-% params={'netContributionNorm2'};
+params={'netContributionNorm2'};
+
 
 % params={'doubleSupportAsym','doubleSupportDiff','stepTimeContributionNorm2','stanceTimeDiff','stepTimeDiff'};
 
-params={'stepTimeContributionNorm2'};%{'stepTimeContributionNorm2'};%{'doubleSupportAsym'};%,,,'stepTimeDiff'};
+% params={'stepTimeDiff'};%{'stepTimeContributionNorm2'};%{'doubleSupportAsym'};%,,'doubleSupportDiff','stepTimeDiff'};
 
 poster_colors;
 colorOrder=[p_red; p_orange; p_fade_green; p_fade_blue; p_plum; p_green; p_blue; p_fade_red; p_lime; p_yellow; [0 0 0];[0 1 1]];
@@ -20,7 +25,7 @@ indivFlag=0; %0 to plot group mean 1 to plot indiv subjects
 indivSubs=[]; %Use when you want to plot a specidfic subject in a group 
 colorOrder=[];%[p_red; p_orange; p_plum;p_fade_green]; %Let the function take care of this at least you wanted in a specific set of color then by my guess and add the list here
 biofeedback= 0; % At least that you are providing with biofeedback to the subject
-removeBiasFlag=1; %if you want to remove bias 
+removeBiasFlag=0; %if you want to remove bias 
 labels=[]; %Groups names 
 filterFlag=[]; 
 plotHandles=[];
