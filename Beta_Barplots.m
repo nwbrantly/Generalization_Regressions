69336cef1dd0d626bdf66dbedbc73bf62dd8da46
DulceMariscal <mariscaldulce@gmail.com>
2022-02-01 15:43:57 -0500

    Update of bootstraps to get the bar plots version of the data

--- Beta_Barplots.m
+++ Beta_Barplots.m
@@ -93,7 +93,8 @@ for i=1:length(groupID)
     legend([b{:}],'\beta_{adapt}','\beta_{noadapt}','\beta_{env}')
     ylabel('|\beta|')
     xticks([2 6 10])
-    xticklabels({'Trans 1: Long Split','Trans 2: Env Changes','Trans 3: Short Split '})
+%     xticklabels({'Trans 1: Long Split','Trans 2: Env Changes','Trans 3: Short Split '})
+    xticklabels({'|\DeltaEMG_{trans1}|','|\DeltaEMG_{trans2}|','|\DeltaEMG_{trans3}|'})
     set(gcf,'color','w');
       
 end
@@ -172,6 +173,58 @@ for tr=1:3
 end
 
 %%
+
+groupID={'ATS','ATR'};
+
+betas={'R^{2}_{Ord}','R^{2}_{Adj}'};
+b=[];
+xx=[1 4 7];
+colorOrder=[[0.4940 0.1840 0.5560];[0.9290 0.6940 0.1250]; [0.4660 0.6740 0.1880]];
+for r=1
+        fh=figure('Units','Normalized','OuterPosition',[0 0 1 1],'NumberTitle', 'off', 'Name',betas{r});
+        x=0;
+for tr=1:3
+    x=xx(tr);
+%         subplot(1,3,tr)
+        hold on
+        for i=1:length(groupID)
+            
+           
+            %     load(['GroupRegression00_',groupID{i},'.mat'])
+            if contains(groupID{i},'TS')
+                load(['/Users/dulcemariscal/Documents/GitHub/Generalization_Regressions/RegressionAnalysis/RegModelResults_', date '/BootstrappingResults/',groupID{i},'_group_iterations_500_numberOfSub_4.mat'])
+                colorFace=colorOrder(1,:);
+                
+            else
+                load(['/Users/dulcemariscal/Documents/GitHub/Generalization_Regressions/RegressionAnalysis/RegModelResults_', date '/BootstrappingResults/',groupID{i},'_group_iterations_500_numberOfSub_4.mat'])
+                colorFace=colorOrder(2,:);
+            end
+            
+            if r==1
+                eval(['b{i}=bar(x,nanmean(Trans' num2str(tr) '.Rsquared_Ord),"FaceColor",colorFace);']);
+                eval(['errorbar(x,nanmean(Trans' num2str(tr) '.Rsquared_Ord),std(Trans' num2str(tr) '.Rsquared_Ord)/sqrt(length(Trans' num2str(tr) '.Rsquared_Ord)),"k");']);
+
+            else
+                eval(['b{i}=bar(x,nanmean(Trans' num2str(tr) '.Rsquared_Adj),"FaceColor",colorFace);']);
+                eval(['errorbar(x,nanmean(Trans' num2str(tr) '.Rsquared_Adj),std(Trans' num2str(tr) '.Rsquared_Adj)/sqrt(length(Trans' num2str(tr) '.Rsquared_Adj)),"k");']);
+            end
+         x=x+1;    
+        end
+        
+   
+end
+    
+%      title(['Trans', num2str(tr)])
+        legend([b{:}],groupID{:})
+        ylabel(betas{r})
+        set(gcf,'color','w');
+         xticks([1.5 4.5 7.5])
+%         xticklabels({'Trans 1: Long Split','Trans 2: Env Changes','Trans 3: Short Split '})
+          xticklabels({'|\DeltaEMG_{trans1}|','|\DeltaEMG_{trans2}|','|\DeltaEMG_{trans3}|'})
+        
+end
+
+%%
 %Norm AF
 groupID={'ATS','ATR'};
 
@@ -196,6 +249,32 @@ legend(groupID{:})
 ylabel('|\DeltaEMG|')
 set(gcf,'color','w');
         
+
+%%
+fh=figure('Units','Normalized','OuterPosition',[0 0 1 1],'NumberTitle', 'off', 'Name','AfterEffects');
+
+hold on
+for i=1:length(groupID)
+    %     load(['GroupRegression00_',groupID{i},'.mat'])
+    if contains(groupID{i},'TS')
+        load(['/Users/dulcemariscal/Documents/GitHub/Generalization_Regressions/RegressionAnalysis/RegModelResults_', date '/BootstrappingResults/',groupID{i},'_group_iterations_500_numberOfSub_4.mat'])
+        colorFace=colorOrder(1,:);
+    else
+        load(['/Users/dulcemariscal/Documents/GitHub/Generalization_Regressions/RegressionAnalysis/RegModelResults_', date '/BootstrappingResults/',groupID{i},'_group_iterations_500_numberOfSub_4.mat'])
+        colorFace=colorOrder(2,:);
+    end
+    
+    b{i}=bar(i,nanmean(Norm_AF),"FaceColor",colorFace);
+    errorbar(i,nanmean(Norm_AF),std(Norm_AF)/sqrt(length(Norm_AF)),'k');
+    
+end
+
+title('|EMG_{AF}|')
+legend([b{:}],groupID{:})
+ylabel('|\DeltaEMG|')
+set(gcf,'color','w');
+xticks([1 2])
+xticklabels({'ATS','ATR'})
 
 %% 
 %Norm regressors 
@@ -228,7 +307,51 @@ for b=1:6
     
 end
 
+fh=figure('Units','Normalized','OuterPosition',[0 0 1 1],'NumberTitle', 'off', 'Name','Regressors');
+% regressors={'Adapt','NoAdapt','EnvSwitch','Trans1','Trans2','Trans3'};
+
+reg=0;
+
+if reg==1
+regressors={'|\DeltaEMG_{adapt}|','|\DeltaEMG_{no-adapt}|','|\DeltaEMG_{env}|'};
+c=1:3;
+else
+regressors={'|\DeltaEMG_{trans1}|','|\DeltaEMG_{trans2}|','|\DeltaEMG_{trans3}|'};
+c=4:6;
+end
 
 
+xx=[1 4 7 1 4 7];
+
+
+
+for b=c
+    %     subplot(1,6,b)
+    hold on
+    x=xx(b);
+    for i=1:length(groupID)
+        %     load(['GroupRegression00_',groupID{i},'.mat'])
+        if contains(groupID{i},'TS')
+            load(['/Users/dulcemariscal/Documents/GitHub/Generalization_Regressions/RegressionAnalysis/RegModelResults_', date '/BootstrappingResults/',groupID{i},'_group_iterations_500_numberOfSub_4.mat'])
+            colorFace=colorOrder(1,:);
+        else
+            load(['/Users/dulcemariscal/Documents/GitHub/Generalization_Regressions/RegressionAnalysis/RegModelResults_', date '/BootstrappingResults/',groupID{i},'_group_iterations_500_numberOfSub_4.mat'])
+            colorFace=colorOrder(2,:);
+        end
+        
+        
+        t{i}=bar(x,nanmean(Norm_Regressors(:,b)),"FaceColor",colorFace);
+        errorbar(x,nanmean(Norm_Regressors(:,b)),std(Norm_Regressors(:,b))/sqrt(length(Norm_Regressors(:,b))),'k');
+        
+        x=x+1;
+    end
+    
+%     title(regressors{b})
+    legend([t{:}],groupID{:})
+    ylabel('|\DeltaEMG|')
+    set(gcf,'color','w');
+    xticks([1.5 4.5 7.5])
+    xticklabels(regressors)
+end
 
 
