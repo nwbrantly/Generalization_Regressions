e25c371ec10b3bd5f433d9a85ee6a6000eccf6ee
DulceMariscal <mariscaldulce@gmail.com>
2022-07-26 13:49:56 -0400

    Update PCA_analysis.m

--- PCA_analysis.m
+++ PCA_analysis.m
@@ -1,10 +1,13 @@
 %  load('ATS_fixD_ShortPostAdapt120422T194824.mat')
  close all
- Y=datSet.out;
+%  Y=datSet.out;
 %  Y=Y(:,1:1150);
+Y=Yasym;
+bias=nanmedian(Yasym(5:30,:));
+Y=Y-bias;
 % [pp,cc,aa]=pca(Y');
 % [pp,cc,aa]=pca(Y');
-[pp,cc,aa]=pca(Y','Centered','off');
+[pp,cc,aa]=pca(Y,'Centered','off');
 %%pp - data projected in the principal component 
 %%cc - Corresponding matrix of eigenvectors
 %%aa - vector of eigent values 
@@ -18,7 +21,7 @@ figure
 subplot(2,1,1)
 plot(aa,'LineWidth',2)
 ylabel('Eigenvalues')
-xline(3,'r')
+xline(2,'r')
 xlabel('Number of Components')
 % ylim([-.5 7])
  axis tight
@@ -29,39 +32,39 @@ subplot(2,1,2)
 csum=cumsum(aa);
 variance_explained = csum / sum(aa);
 plot(variance_explained,'LineWidth',2)
-xline(3,'r')
+xline(2,'r')
 ylabel('Variance explained')
 xlabel('Number of Components')
 set(gcf,'color','w')
 %% Using first 2 components to estimate the states 
 
-C=[pp(:,1:3)];
+C=[pp(:,1:2)];
 Cinv=pinv(C)';
-X= Y'*Cinv;
+X= Y*Cinv;
 
 figure
-subplot(3,1,1)
+subplot(2,1,1)
 plot(movmean(X(:,1),5),'LineWidth',2)
 yline(0)
 title('C_1 = PCA1')
 % legend('ATR','ATS')
 
-subplot(3,1,2)
-
+subplot(2,1,2)
 plot(movmean(X(:,2),5),'LineWidth',2)
 yline(0)
 title('C_2 = PCA2')
-
-subplot(3,1,3)
-plot(movmean(X(:,3),5),'LineWidth',2)
-yline(0)
-title('C_3 = PCA3')
+% 
+% subplot(3,1,3)
+% plot(movmean(X(:,3),5),'LineWidth',2)
+% yline(0)
+% title('C_3 = PCA3')
 % legend('ATR','ATS')
 
 set(gcf,'color','w')
 %%
 model.C=C;
-legacy_vizSingleModelMLMC_FreeModel(model,datSet.out,datSet.in)
-
-model.C=C(:,1:2);
-legacy_vizSingleModelMLMC_FreeModel(model,datSet.out,datSet.in)
+legacy_vizSingleModel_FreeModel_ShortAdaptation(model,Yasym',Uf)
+% legacy_vizSingleModelMLMC_FreeModel(model,datSet.out,datSet.in)
+% 
+% model.C=C(:,1:2);
+% legacy_vizSingleModelMLMC_FreeModel(model,datSet.out,datSet.in)
