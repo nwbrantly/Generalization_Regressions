f4796c76103a0926d2f8149c1bc7c1daa321a0cd
DulceMariscal <mariscaldulce@gmail.com>
2022-11-09 10:50:28 -0500

    Update preProcessData_LinearFit.m

--- preProcessData_LinearFit.m
+++ preProcessData_LinearFit.m
@@ -5,7 +5,7 @@
 clear; close all; clc;
 
 % set script parameters, SHOULD CHANGE/CHECK THIS EVERY TIME.
-groupID = 'PATR';
+groupID = 'BATS';
 scriptDir = cd;% fileparts(matlab.desktop.editor.getActiveFilename);
 files = dir ([groupID '*params.mat']);
 
@@ -45,14 +45,16 @@ baseEp=getBaseEpoch;
 %Adaptation epochs
 % strides=[-150 300 300 300 600];exemptFirst=[0];exemptLast=[0];
 % strides=[-50 900 300];
-splits=1;
+splits=0;
 if splits==1
     strides=[-40 980 100];
     cond={'TM base','Multiple pos short splits','TM mid 2'}; %Conditions for this group
 else
     
-    strides=[-40 450 200];
+    strides=[-40 440 200];
     cond={'TM base','Adaptation','Post 1'}; %Conditions for this group
+%     strides=[50];
+%     cond={'Post 1'}; %Conditions for this group
 end
 exemptFirst=[1];
 exemptLast=[5]; %Strides needed 
@@ -67,8 +69,9 @@ if contains(groupID,'NTS') || contains(groupID,'NTR') || contains(groupID,'CTS')
     strides=[-50 600 150];
 end
 ep=defineEpochs(cond,cond,strides,exemptFirst,exemptLast,'nanmean',{'Base','Adapt','Post1'}); %epochs 
+% ep=defineEpochs(cond,cond,strides,exemptFirst,exemptLast,'nanmean',{'Post1'}); %epochs 
 %% Define params we care about:
-mOrder={'TA', 'PER', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'HIP','TFL', 'GLU'};
+mOrder={'TA','PER', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'HIP','TFL', 'GLU'};
 nMusc=length(mOrder);
 type='s';
 labelPrefix=fliplr([strcat('f',mOrder) strcat('s',mOrder)]); %To display
@@ -77,7 +80,8 @@ labelPrefixLong= strcat(labelPrefix,['_' type]); %Actual names
 % %Adding alternative normaliza tion parameters:
 l2=group.adaptData{1}.data.getLabelsThatMatch('^Norm');
 group=group.renameParams(l2,strcat('N',l2)).normalizeToBaselineEpoch(labelPrefixLong,baseEp,true); %Normalization to max=1 but not min=0
-% 
+
+
 % %Renaming normalized parameters, for convenience:
 ll=group.adaptData{1}.data.getLabelsThatMatch('^Norm');
 l2=regexprep(regexprep(ll,'^Norm',''),'_s','s');
@@ -95,6 +99,7 @@ end
 %% get data:
 padWithNaNFlag=true;
 [dataEMG,labels,allDataEMG]=group.getPrefixedEpochData(newLabelPrefix,ep,padWithNaNFlag);
+
 %Flipping EMG:
 for i=1:length(allDataEMG)
     aux=reshape(allDataEMG{i},size(allDataEMG{i},1),size(labels,1),size(labels,2),size(allDataEMG{i},3));
@@ -112,6 +117,7 @@ SLA=squeeze(cell2mat(dataContribs));
 h5create(name,'/SLA',size(SLA))
 h5write(name,'/SLA',SLA)
 speedDiff=[zeros(1,abs(strides(1))),ones(1,strides(2)),zeros(1,(strides(3)))];
+% speedDiff=[ones(1,strides(1))];
 h5create(name,'/speedDiff',size(speedDiff))
 h5write(name,'/speedDiff',speedDiff)
 breaks=[zeros(1,length(speedDiff))];
