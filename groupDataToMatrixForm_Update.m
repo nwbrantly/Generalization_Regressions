5e3429d9f3e26af0e99df64228099f1a6598c733
DulceMariscal <mariscaldulce@gmail.com>
2023-06-01 16:22:43 -0400

    Update groupDataToMatrixForm_Update.m
    - Adding label output
    - Output of individual subject with both legs

--- groupDataToMatrixForm_Update.m
+++ groupDataToMatrixForm_Update.m
@@ -1,12 +1,5 @@
-function [Y,Yasym,Ycom,U,Ubreaks,Ysum]=groupDataToMatrixForm_Update(subjIdx,fName,sqrtFlag)
+function [Y,Yasym,Ycom,U,Ubreaks,Ysum,Yindv,labels]=groupDataToMatrixForm_Update(subjIdx,fName,sqrtFlag)
 %% Load real data:
-% fName='dynamicsData_C_s12V2.h5';
-% fName='dynamicsData_AUF_s7.h5';
-% fName='dynamicsData_NTS.h5';
-% fName='dynamicsData_CTR.h5';
-% fName='dynamicsData_PATS.h5'
-% fName='dynamicsData_ATR_V4.h5';
-% fName='dynamicsData_ATS_V6.h5';
 EMGdata=h5read(fName,'/EMGdata');
 SLA=h5read(fName,'/SLA');
 speedDiff=h5read(fName,'/speedDiff');
@@ -22,8 +15,10 @@ Ubreaks=breaks;
 %     subjIdx=2:16; %Excluding C01 only
 % end
 %%
-muscPhaseIdx=1:size(EMGdata,2);%336; %14 muscles per leg %360; %All muscles
+muscPhaseIdx=1:size(EMGdata,2);
 Y=EMGdata(:,muscPhaseIdx,subjIdx);
+% Yindv=nanmedian(Y,3);
+Yindv=Y;
 
 if nargin>2 && sqrtFlag
     Y=sqrt(Y);
@@ -34,5 +29,5 @@ Yasym=Yasym(:,1:size(Yasym,2)/2,:);
 Ysum=Y+fftshift(Y,2);
 Ysum=Ysum(:,1:size(Ysum,2)/2,:);
 Ycom=Ycom(:,1:size(Ycom,2)/2,:);
-Y=nanmedian(Yasym,3); %Median across subjs
+Y=nanmedian(Y,3); %Median across subjs
 end
