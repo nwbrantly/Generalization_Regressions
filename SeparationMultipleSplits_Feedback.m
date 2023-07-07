057193bbe4e35af87ddae0880b227eca4da9ffd7
DulceMariscal <mariscaldulce@gmail.com>
2023-07-07 11:58:25 -0400

    Update SeparationMultipleSplits_Feedback.m
    - Adding description of the script and comments

--- SeparationMultipleSplits_Feedback.m
+++ SeparationMultipleSplits_Feedback.m
@@ -1,16 +1,18 @@
-% Script to separate the conditions 
+% Script to separate the conditions
+% In the EMG genralization protocol we did not stop the treadmill between
+% transitions. Thus, we are separating this epochs in a post-hoc manner.
+% this is a script intended for indvdal params files 
 %% Load params file 
 subID='BATR11'
 load([subID, 'params.mat'])
 
-%%
-
-%Adaptation 
+%% %Adaptation 
 load([subID, 'params.mat'])
 adaptData = AddingConditions(adaptData, 'Adaptation', 'Adaptation2', true, 'Tied to Split');
 save([subID, 'params.mat'],'adaptData')
 changeCondName(subID,{'Adaptation', 'Adaptation2'},{'TM base','Adaptation'})
-%%
+
+%% Short perturbations 
 % Tied to Pos Short 
 load([subID, 'params.mat'])
 adaptData = AddingConditions(adaptData, 'Pos Short', 'Pos short 2', true, 'Tied to Split');
@@ -24,25 +26,21 @@ save([subID, 'params.mat'],'adaptData')
 
 % Tied to Neg Short 
 load([subID, 'params.mat'])
-adaptData = AddingConditions(adaptData,'Neg Short','Neg short 2', true, 'Tied to Split');
-% adaptData = AddingConditions(adaptData, 'Tied Neg', 'Neg short', true, 'Tied to Split');
+adaptData = AddingConditions(adaptData, 'Tied Neg', 'Neg short', true, 'Tied to Split');
 save([subID, 'params.mat'],'adaptData')
 changeCondName(subID,{'Neg Short', 'Neg short 2'},{'Tied Neg', 'Neg short'})
 
-%Neg Short  to tied
+% Neg Short  to tied
 load([subID, 'params.mat'])
 adaptData = AddingConditions(adaptData, 'Neg short', 'Tied post Neg', false, 'Tied to Split');
 save([subID, 'params.mat'],'adaptData')
-%
 
 % Ramp
 load([subID, 'params.mat'])
 adaptData = AddingConditions(adaptData, 'Pos Short Ramp', 'Tied post ramp', false, 'Tied to Split');
 save([subID, 'params.mat'],'adaptData')
 
-% adaptData = AddingConditions(adaptData, 'Pos short ramp', 'Tied post ramp', false, 'Split to tied');
-%%
-% Mulitple short 
+%% Mulitple short 
 adaptData = AddingConditions(adaptData, 'Multiple Pos Shorts Splits', 'Split Pos 1', true, 'Tied to Split');
 
 for t= 1:18
@@ -58,24 +56,3 @@ for t= 1:18
     end
 end
 save([subID, 'params.mat'],'adaptData')
-
-%%
-% adaptData = AddingConditions(adaptData, 'Multiple pos short splits 2', 'Split Pos 10', true, 'Tied to Split');
-% changeCondName(subID,{'Multiple pos short splits 2'},{'Tied 9'})
-% 
-% % adaptData = AddingConditions(adaptData, 'Pos short ramp', 'Tied post ramp', false, 'Split to tied');
-% %%
-% 
-% for t= 11:20
-%     if rem(t, 2) == 0
-%         adaptData = AddingConditions(adaptData, ['Tied Pos ' num2str(t-1)], ['Split Pos ' num2str(t)], true, ['Tied to Split', num2str(t)]);
-%         
-%     else
-%         if t==1
-%             adaptData = AddingConditions(adaptData, ['Split Pos ' num2str(t)], ['Tied Pos ' num2str(t)], false, ['Split to Tied', num2str(t)]);
-%         else
-%             adaptData = AddingConditions(adaptData, ['Split Pos ' num2str(t-1)], ['Tied Pos ' num2str(t)], false, ['Split to Tied', num2str(t)]);
-%         end
-%     end
-% end
-% save([subID, 'params.mat'],'adaptData')
