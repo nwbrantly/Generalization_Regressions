41dbd292c3d9bff409b30f4b678cdd86240e7fd1
DulceMariscal <mariscaldulce@gmail.com>
2023-07-07 16:19:26 -0400

    Update checkerboards_example.m
    - Add example with bias remove

--- DataVisualization/checkerboards_example.m
+++ DataVisualization/checkerboards_example.m
@@ -1,24 +1,38 @@
-groupID ='BATR';
-[normalizedGroupData, newLabelPrefix,n_subjects]=creatingGroupdataWnormalizedEMG(groupID);
+groupID ='BATR'; %name of the groups that you want to plot 
 
-ep=defineRegressorsDynamicsFeedback('nanmean');
+[normalizedGroupData, newLabelPrefix,n_subjects]=creatingGroupdataWnormalizedEMG(groupID); % Get normalized data as defult is normalizing by TM base 
 
+ep=defineRegressorsDynamicsFeedback('nanmean'); %Defining epochs of interest. This needs to be costum to your study 
+
+% Epochs that you want to plot 
 OGbase=defineReferenceEpoch('OG base',ep);
 TMbase= defineReferenceEpoch('TM base',ep);
-Pos1_Late=defineReferenceEpoch('Post1_{early}',ep);
-Pos2_Late=defineReferenceEpoch('Post2_{early}',ep);
+Pos1_Late=defineReferenceEpoch('Post1_{Early}',ep);
+Pos2_Late=defineReferenceEpoch('Post2_{Early}',ep);
 
 EpochsOfInteres={OGbase,TMbase,Pos1_Late,Pos2_Late};
 
-plotIndSubjects=1
- plotGroup=1
+
+plotIndSubjects=0; % If you want tp plot individual subjects  
+plotGroup=1; %If you want to plot the group average 
+
+%NOTE removebias is 1 you need to define your refence 
+
+removeBias=1; % We are choosing to remove bias 
+ref=TMbase; %This is the epoch that we are going to use as reference 
+
+if removeBias==1
+    if isempty(ref)
+        disp('You need to define your refence')
+    end
+end
 
 if plotIndSubjects
-    plotEpochsPlusNorm(EpochsOfInteres,normalizedGroupData,newLabelPrefix,1,0)
+    plotEpochsPlusNorm(EpochsOfInteres,normalizedGroupData,newLabelPrefix,plotIndSubjects,0,removeBias,ref)
 end
 
 if plotGroup
-    plotEpochsPlusNorm(EpochsOfInteres,normalizedGroupData,newLabelPrefix,0,1)
+    plotEpochsPlusNorm(EpochsOfInteres,normalizedGroupData,newLabelPrefix,0,plotGroup,removeBias,ref)
     
 end
 
