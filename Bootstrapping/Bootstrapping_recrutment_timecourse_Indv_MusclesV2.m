6a37f9738c3eb1b024d3a45c1167a34769882673
DulceMariscal <mariscaldulce@gmail.com>
2023-07-12 15:58:45 -0400

    Revert "add condition"
    This reverts commit 3c3848d46c74f7a433c61d61800941a0a10c778c.

--- Bootstrapping/Bootstrapping_recrutment_timecourse_Indv_MusclesV2.m
+++ Bootstrapping/Bootstrapping_recrutment_timecourse_Indv_MusclesV2.m
@@ -3,7 +3,6 @@
 
 %%
 % upload your path 
-% upload your path
 main='/Users/dulcemariscal/Documents/GitHub/';
 % main='C:\Users\dum5\OneDrive - University of Pittsburgh\_BoxMigration\GitHub\';
 addpath(genpath([main, 'Generalization_Regressions']))
@@ -19,14 +18,6 @@ clear all; close all; clc;
 % clear all; clc;
 groupID ={'BATR','BATS'};
 EMG2=[];
-adaptationonly=0
-removeMuscles=1
-
-if adaptationonly==1
-    groupID ={'BAT'};
-else
-    groupID ={'BATR','BATS'};
-end
 
 for id=1:length(groupID)
     
