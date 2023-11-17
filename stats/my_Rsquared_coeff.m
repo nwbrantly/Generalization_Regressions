485a997aa6388c53e2b2b3f49f96c6d488195c83
DulceMariscal <mariscaldulce@gmail.com>
2023-11-17 10:13:25 -0500

    Update my_Rsquared_coeff.m
    - Adding parameter to handle non-centered R2 values

--- stats/my_Rsquared_coeff.m
+++ stats/my_Rsquared_coeff.m
@@ -1,14 +1,20 @@
-function Rsquared = my_Rsquared_coeff(data,data_fit)
+function Rsquared = my_Rsquared_coeff(data,data_fit,relativeToMean)
     % R2 correlation coefficient computation
     
     % The total sum of squares
-    sum_of_squares = sum((data-mean(data)).^2);
+    if relativeToMean
+        sum_of_squares = sum((data-mean(data)).^2);
+    else
+        sum_of_squares = sum((data).^2);
+    end
     
-    % The sum of squares of residuals, also called the residual sum of squares:
+    % The sum of squares of residuals, also called the residual sum of squares
     sum_of_squares_of_residuals = sum((data-data_fit).^2);
+
     
     % definition of the coefficient of correlation is
     Rsquared = 1 - sum_of_squares_of_residuals./sum_of_squares;
+
     
     
 end
