bab6877fc277e30fd34aa8ec668f199df3fcca10
DulceMariscal <mariscaldulce@gmail.com>
2024-04-02 14:20:09 -0400

    Add reference condition
    Updates:
    - Input reference condition
    - Output of subject ID

--- creatingGroupdataWnormalizedEMG.m
+++ creatingGroupdataWnormalizedEMG.m
@@ -1,4 +1,4 @@
-function [GroupData, newLabelPrefix,n_subjects]=creatingGroupdataWnormalizedEMG(groupID,invMuscles)
+function [GroupData, newLabelPrefix,n_subjects,subID]=creatingGroupdataWnormalizedEMG(groupID,invMuscles,refEp)
 %This function is to created the group data and also normalize the EMG. In
 %this version I am normalizing to TM base (tied belt right before
 %adpatation). We are normalizing everthing by TM base for consistance
@@ -15,35 +15,52 @@ for i =1:n_subjects
     subID{ii} = sub{ii}(1:end-10);
 end
 
-subID % This display what are the ID of the partocopants that you are using. 
+subID % This display what are the ID of the participants that you are using.
 
-%%
+%% load and prep data
 
-%%%% load and prep data
 muscleOrder={'TA', 'PER', 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT', 'VM', 'VL', 'RF', 'HIP','TFL', 'GLU'};
 %%%% load and prep data Posterior muscles
 % muscleOrder={ 'SOL', 'LG', 'MG', 'BF', 'SEMB', 'SEMT'};
-newLabelPrefix = defineMuscleList(muscleOrder); %List of muscle 
+newLabelPrefix = defineMuscleList(muscleOrder); %List of muscle
 n_muscles=length(muscleOrder)*2;
 
-
-if nargin>1 
-    if invMuscles==1 %For the prepocess code this needs to run bc we need to organize the data correctly 
-    newLabelPrefix=newLabelPrefix([n_muscles:-1:n_muscles/2+1 n_muscles/2:-1:1]);
+if nargin>1
+    if invMuscles==1 %For the prepocess code this needs to run bc we need to organize the data correctly
+        newLabelPrefix=newLabelPrefix([n_muscles:-1:n_muscles/2+1 n_muscles/2:-1:1]);
     end
 end
 
-ep=defineRegressorsDynamicsFeedback('nanmedian'); %loading the variables for multiple epochs of interest
-refEpTM = defineReferenceEpoch('TM base',ep); %extracting the info for the epoch that want to use to normalize the data
- 
+if nargin>2
+    
+    if isempty(refEp)
+        
+        if contains(groupID,'BAT')
+            ep=defineRegressorsDynamicsFeedback('nanmedian'); %loading the variables for multiple epochs of interest
+            refEp = defineReferenceEpoch('TM base',ep); %extracting the info for the epoch that want to use to normalize the data
+        elseif contains(groupID,'CTS') || contains(groupID,'CTR') || contains(groupID,'NTS') || contains(groupID,'NTR') ...
+                || contains(groupID,'VATS') || contains(groupID,'VATR')
+            ep=defineEpochNimbusShoes('nanmean'); %loading the variables for multiple epochs of interest
+            refEp = defineReferenceEpoch('TRbase',ep); %extracting the info for the epoch that want to use to normalize the data
+        elseif contains(groupID,'C3')
+            ep=defineRegressors_StrokeC3('nanmedian'); %loading the variables for multiple epochs of interest
+            refEp = defineReferenceEpoch('TM base',ep); %extracting the info for the epoch that want to use to normalize the data
+            
+        elseif contains(groupID,'MWS')
+            ep= defineEpochMW('nanmean');
+            refEp = defineReferenceEpoch('OGbase',ep);
+        end
+    end
+end
+
 GroupData=adaptationData.createGroupAdaptData(sub); %loading the data
 GroupData=GroupData.removeBadStrides; %Removing bad strides
 
 
-GroupData = GroupData.normalizeToBaselineEpoch(newLabelPrefix,refEpTM,true); %Normalizing the data
-ll=GroupData.adaptData{1}.data.getLabelsThatMatch('^Norm'); %Getting the label witht the word Norm 
+GroupData = GroupData.normalizeToBaselineEpoch(newLabelPrefix,refEp,true); %Normalizing the data
+ll=GroupData.adaptData{1}.data.getLabelsThatMatch('^Norm'); %Getting the label witht the word Norm
 l2=regexprep(regexprep(ll,'^Norm',''),'_s','s'); %Removing the word norm. This is just to make our life easier
-GroupData=GroupData.renameParams(ll,l2); %Rename the variable to not have the word Norm 
+GroupData=GroupData.renameParams(ll,l2); %Rename the variable to not have the word Norm
 newLabelPrefix = regexprep(newLabelPrefix,'_s','s'); %removing the underscore "_"
 
 end
