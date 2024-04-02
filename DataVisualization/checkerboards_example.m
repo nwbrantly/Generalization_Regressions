80ceaf78d5b8692b7366a45be4620813fa921c2c
DulceMariscal <mariscaldulce@gmail.com>
2024-04-02 09:50:22 -0400

    Script with example of plotting the checkerboards

--- DataVisualization/checkerboards_example.m
+++ DataVisualization/checkerboards_example.m
@@ -1,26 +1,34 @@
-groupID ='BATR'; %name of the groups that you want to plot 
+close all;clear all; clc
+
+groupID ='NTS'; %name of the groups that you want to plot 
 
 [normalizedGroupData, newLabelPrefix,n_subjects]=creatingGroupdataWnormalizedEMG(groupID); % Get normalized data as defult is normalizing by TM base 
 
-ep=defineRegressorsDynamicsFeedback('nanmean'); %Defining epochs of interest. This needs to be costum to your study 
+% ep=defineRegressors_StrokeC3('nanmean'); %Defining epochs of interest. This needs to be costum to your study 
 
+ep=defineEpochNimbusShoes('nanmean'); 
 % Epochs that you want to plot 
-OGbase=defineReferenceEpoch('OG base',ep);
-TMbase= defineReferenceEpoch('TM base',ep);
-Pos1_Late=defineReferenceEpoch('Post1_{Early}',ep);
-Pos2_Late=defineReferenceEpoch('Post2_{Early}',ep);
-
-EpochsOfInteres={OGbase,TMbase,Pos1_Late,Pos2_Late};
-
-
+OGbase=defineReferenceEpoch('OGbase',ep);
+TRbase= defineReferenceEpoch('TRbase',ep);
+% EarlyAdapt= defineReferenceEpoch('Adaptation_{early}',ep);
+LateAdapt= defineReferenceEpoch('Adaptation',ep);
+Pos1_early=defineReferenceEpoch('Post1_{Early}',ep);
+% Pos2_early=defineReferenceEpoch('Post2_{Early}',ep);
+% % Optimal=defineReferenceEpoch('Optimal',ep);
+NegShort=defineReferenceEpoch('SplitNeg',ep);
+% PostShort= defineReferenceEpoch('PosShort_{late}',ep);
+% EpochsOfInteres={OGbase,TMbase,EarlyAdapt,LateAdapt,Pos1_Late,Pos2_Late};
+%%
+% EpochsOfInteres={TRbase,NegShort,LateAdapt};
+EpochsOfInteres={OGbase,TRbase,Pos1_early};
 plotIndSubjects=0; % If you want tp plot individual subjects  
 plotGroup=1; %If you want to plot the group average 
 
 %NOTE removebias is 1 you need to define your refence 
 
 removeBias=1; % We are choosing to remove bias 
-ref=[]; %This is the epoch that we are going to use as reference 
-
+ref=OGbase; %This is the epoch that we are going to use as reference 
+%  ref=TRbase; %This is the epoch that we are going to use as reference 
 if removeBias==1
     if isempty(ref)
         warning('You need to define your refence')
@@ -33,7 +41,7 @@ if plotIndSubjects
 end
 
 if plotGroup
-    plotEpochsPlusNorm(EpochsOfInteres,normalizedGroupData,newLabelPrefix,0,plotGroup,removeBias,ref)
+    Data= plotEpochsPlusNorm(EpochsOfInteres,normalizedGroupData,newLabelPrefix,0,plotGroup,removeBias,ref)
     
 end
 
